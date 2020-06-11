beb3bd758612d66a0bc32853e5bb57391cc88a4c
Vladislav Holovko <lobsterponi@gmail.com>
2020-06-11 20:15:35 +0300

    Updates

---
+++ precache-manifest.2eae5e7caf0efb93ba1eee65e211c500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54be170b5332a99d723f1a588b21f0ef",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "872fba6fa57238d0e8ee",
+    "url": "/minesweeper/static/css/main.4471ab8d.chunk.css"
+  },
+  {
+    "revision": "8cf672b3410ee293540f",
+    "url": "/minesweeper/static/js/2.0105c404.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.0105c404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872fba6fa57238d0e8ee",
+    "url": "/minesweeper/static/js/main.63b39a69.chunk.js"
+  },
+  {
+    "revision": "2cf7e9a7706d6861c21b",
+    "url": "/minesweeper/static/js/runtime-main.a397ea82.js"
+  }
+]);
